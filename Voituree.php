d475e643c64b96afb30c296bcdf1735d026e69c0
Joseph-Laugier <112881590+Joseph-Laugier@users.noreply.github.com>
2022-12-13 09:37:31 +0100

    Add files via upload

---
+++ Voituree.php
@@ -0,0 +1,105 @@
+<?php
+class Voiture { 
+ private $marque;
+ private $couleur;
+ private $immatriculation;
+
+ // getter
+ public function getMarque() {
+ return $this->marque;
+ }
+
+public function getCouleur() {
+ return $this->couleur;
+ }
+
+ public function getImmatriculation() {
+ return $this->immatriculation;
+ }
+
+  public static function getAllVoitures(){
+    require_once "Model.php";
+    $bdd = Model::Init();
+    $rep = $bdd-> query("SELECT * FROM Voiture");
+    $rep ->setFetchMode(PDO::FETCH_CLASS,'Voiture');
+    $tab_voit = $rep->fetchAll();
+
+      foreach ($tab_voit as $row) {
+        $row->afficher();
+      }
+  }
+
+  public static function getVoitureByImmat($immat){
+    $sql = "SELECT * FROM voiture WHERE immatriculation=:nom_tag";
+
+    $req_prep = Model::$pdo->prepare($sql);
+    $values = array("nom_tag" => $immat);
+
+    $req_prep->execute($values);
+    $req_prep->setFetchMode(PDO::FETCH_CLASS,'Voiture');
+    $tab_voit = $req_prep->fetchAll();
+
+    if (empty($tab_voit))
+      return false;
+    return$tab_voit[0];
+      }
+
+ // setter
+ public function setMarque($marque2) {
+ $this->marque = $marque2;
+ }
+
+  public function setCouleur($couleur2) {
+ $this->marque= $marque2;
+ }
+
+ public function setImmatriculation($immatriculation2) {
+ if(strlen($immatriculation) ==8)
+{
+  $this->immatriculation = $immatriculation2;
+
+}else{
+	echo"une plaque d'immatriculation doit avoir 8 characters";
+}
+}
+
+
+public static function save($v){
+  $sql = "INSERT INTO voiture (immatriculation, marque, couleur) VALUES (:mimmatriculation, :marque, :couleur)";
+  $req_prep = Model::$pdo->prepare($sql);
+  $values = array(
+    "immatriculation" => $v->getImmatriculation(),
+    "marque" => $v->getMarque(),
+    "couleur" => $v->getCouleur(),
+  );
+  $req_prep->execute($values);
+}
+
+ //un constructeur
+ public function __construct($m = NULL, $c = NULL, $i = NULL) {
+    if(!is_null($m) && !is_null($c) && !is_null($i)) {
+       $this->marque = $m;
+       $this->couleur = $c;
+       $this->immatriculation = $i;
+    }
+ 
+ }
+
+
+ // une methode d'affichage.
+ public function afficher() {
+ echo'
+  Info:
+  <ul>
+      <li> Marque: '.$this->getMarque().'</li>
+      <li> Couleur: '.$this->getCouleur().'</li>
+      <li> Immatriculation: '.$this->getImmatriculation().'</li>
+  </ul>
+
+ ';
+
+
+ }
+}
+
+?>
